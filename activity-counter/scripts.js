fdeaa3f6015ee284a5f3c37b8bc44486489ac1bb
Matthias Mosthof <matthias.mosthof@gmail.com>
2022-12-06 09:15:27 +0100

    refactor: minor-changes

--- activity-counter/scripts.js
+++ activity-counter/scripts.js
@@ -1,38 +1,131 @@
 Vue.createApp({
   data() {
     return {
-      secondsIn: 0,
-      minutesIn: 0,
-      hoursIn: 0,
+      addTimes: [
+        {
+          activityName: "",
+          secondsIn: 0,
+          minutesIn: 0,
+          hoursIn: 0,
+          active: false,
+        },
+      ],
+      times: [
+        {
+          activityName: "VueJS",
+          secondsIn: 0,
+          minutesIn: 0,
+          hoursIn: 0,
+          active: false,
+        },
+      ],
+
       intervalSeconds: null,
       intervalMinutes: null,
       intervalHours: null,
+      intervalServerRequest: null,
     };
+  },
+  async created() {
+    const response = await fetch("http://localhost:3000/activity");
+    this.times = await response.json();
   },
   computed: {},
   methods: {
     timer(event) {
-      console.log("hello");
       if (event.target.checked === true) {
-        this.intervalSeconds = setInterval(this.countSeconds, 1000);
-        this.intervalMinutes = setInterval(this.countMinutes, 60000);
-        this.intervalHours = setInterval(this.countHours, 36000000);
+        this.times.forEach((activity) => {
+          if (
+            event.path[2].firstElementChild.childNodes[1].innerText ===
+            activity.activityName
+          ) {
+            activity.active = true;
+          }
+        });
+        this.times.forEach((activity) => {
+          if (activity.active === true) {
+            this.intervalSeconds = setInterval(this.countSeconds, 1000);
+            this.intervalMinutes = setInterval(this.countMinutes, 1000);
+            this.intervalHours = setInterval(this.countHours, 1000);
+            this.intervalHours = setInterval(this.serverRequest, 1000);
+          }
+        });
       }
+
       if (event.target.checked === false) {
+        this.times.forEach((activity) => {
+          if (
+            event.path[2].firstElementChild.childNodes[1].innerText ===
+            activity.activityName
+          ) {
+            activity.active = false;
+          }
+        });
         clearInterval(this.intervalSeconds);
         clearInterval(this.intervalMinutes);
         clearInterval(this.intervalHours);
+        clearInterval(this.intervalServerRequest);
+        clearInterval(this.intervalServerRequest);
+        //this.serverRequest();
       }
     },
 
     countSeconds() {
-      this.secondsIn++;
+      /*this.times[0].secondsIn++;
+      console.log(this.times[0].secondsIn);
+      
+      }*/
+      this.times.forEach((activity) => {
+        if (activity.active === true) {
+          activity.secondsIn++;
+          if (this.times[0].secondsIn >= 60) {
+            this.times[0].secondsIn = 0;
+          }
+        }
+      });
     },
+
     countMinutes() {
-      this.minutesIn++;
+      if (this.times[0].secondsIn === 0) {
+        this.times[0].minutesIn++;
+      }
+      if (this.times[0].minutesIn === 60) {
+        this.times[0].minutesIn = 0;
+      }
     },
+
     countHours() {
-      this.hoursIn++;
+      if (this.times[0].minutesIn === 0 && this.times[0].secondsIn === 0) {
+        this.times[0].hoursIn++;
+      }
+    },
+    async serverRequest() {
+      let activity = this.times[0];
+
+      await fetch("http://localhost:3000/activity/1", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(activity),
+      });
+    },
+    safeInput(event) {
+      this.addTimes[0].activityName = event.target.value;
+    },
+    renderActivity() {
+      fetch("http://localhost:3000/activity", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(this.addTimes[0]),
+      });
+      this.loadActivity();
+    },
+    async loadActivity() {
+      const response = await fetch("http://localhost:3000/activity");
+      this.times = await response.json();
     },
   },
 }).mount("#app");
